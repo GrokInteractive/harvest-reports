3802efb3b414a3e211233f621ab9a468ea4bdf08
Josh Freeman <jdfreeman11@gmail.com>
2016-12-18 18:15:54 -0600

    Setup correct directories for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,20 @@ require 'harvest_reports'
 require 'minitest'
 require 'minitest/autorun'
 require 'pry'
+
+class Minitest::Test
+
+  def setup
+    super
+    Client.filename = './test/download/clients.json'
+    Project.filename = './test/download/projects.json'
+    User.filename = './test/download/users.json'
+  end
+
+  def teardown
+    super
+    Client.filename = './test/download/clients.json'
+    Project.filename = './test/download/projects.json'
+    User.filename = './test/download/users.json'
+  end
+end
